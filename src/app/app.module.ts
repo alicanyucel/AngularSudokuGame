ba33ef71214c6d21382a04371da82b2ec5c22c0c
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-16 23:16:09 +0300

    ....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SudokuComponent } from './components/sudoku/sudoku.component';
+import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,7 +14,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+  FormsModule,
+  AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
